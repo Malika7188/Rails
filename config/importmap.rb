20cc96ec7ecd58a320f301f43ee5a92f5c1910cc
Malika7188 <your-email@example.com>
2025-04-08 00:16:38 +0300

    config import

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# config/importmap.rb
+pin "application"
+pin "leaflet", to: "https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js" # Keep only this line
+pin "react" # @19.1.0
+pin "react-dom" # @19.1.0
