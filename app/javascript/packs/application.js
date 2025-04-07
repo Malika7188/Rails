2d8362f9339d94e152a8bef8a064ce615054b6d1
Malika7188 <your-email@example.com>
2025-04-07 23:59:57 +0300

    js pack

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,28 @@
+// app/javascript/application.js
+import '@hotwired/turbo-rails'
+import * as ActiveStorage from '@rails/activestorage'
+import 'controllers'
+
+// Leaflet initialization
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+})
+
+// Initialize Inertia
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+
+createInertiaApp({
+  resolve: name => import(`./components/${name}.jsx`),
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  }
+})
+
+ActiveStorage.start()
