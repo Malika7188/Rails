05e675209807a617e7dc3e071d4b61ad20177bc3
Malika7188 <your-email@example.com>
2025-04-07 23:40:19 +0300

    application

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Redirect users after sign in or sign up
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+end
