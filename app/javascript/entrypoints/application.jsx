4fa3f3e56334f4e6cbe21bafbe11e97e0fd6b2b7
Malika7188 <your-email@example.com>
2025-04-08 03:49:46 +0300

    updates

--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -10,13 +10,13 @@ import { createRoot } from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
-delete L.Icon.Default.prototype._getIconUrl
+// import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/vite-dev/assets/marker-icon-2x-7a2d058b.png',
-  iconUrl: '/vite-dev/assets/marker-icon-9d89b3a8.png',
-  shadowUrl: '/vite-dev/assets/marker-shadow-0c510a80.png'
-})
-
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
 createInertiaApp({
   resolve: name => import(`../components/${name}.jsx`),
   setup({ el, App, props }) {
